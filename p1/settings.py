77c08f59c9a44b9e7514affe25b66a299ac7e756
christttto <christttto@berkeley.edu>
2018-06-24 03:28:13 -0400

    first

--- p1/settings.py
+++ p1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dh)m$rf)%qu#%pvyo=7b8gipl6p$i%v6l=_-mxoja$ylc!4rny'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["p1-christttto-christttto714677.codeanyapp.com"]
 
 
 # Application definition
